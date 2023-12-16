49cbebcd3f1619e6b36ee29dcf2781dcb5bbce6f
Brandon <brandon.aron38@gmail.com>
2023-12-16 15:46:10 +0200

    Added Script and other project files

---
+++ Morning_Routine.py
@@ -0,0 +1,31 @@
+import webbrowser
+import os
+
+
+# URLs to open in Microsoft Edge
+urls = [
+    "https://chat.openai.com/",         # ChatGPT
+    "https://app.daily.dev/",           # Daily.dev
+    "https://www.leetcode.com/",        # LeetCode
+    "https://www.github.com/",          # GitHub
+]
+
+# Open URLs in Microsoft Edge
+for url in urls:
+    webbrowser.open_new_tab(url)
+
+# Launch Microsoft To Do
+try:
+    os.startfile("ms-todo:")
+except Exception as e:
+    print(f"Failed to open Microsoft To Do: {e}")
+# Launch Outlook
+try:
+    os.startfile("outlookcal:")
+except Exception as e:
+    print(f"Failed to open Outlook: {e}")
+# Launch Spotify
+try:
+    os.startfile("spotify:")
+except Exception as e:
+    print(f"Failed to open Spotify: {e}")
